00fce29e32d067f5e2715993aac886da4365814d
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2016-05-23 21:49:38 +0300

    -Init Commit

---
+++ ColorGame/ColorGame/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ColorGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.panelPlayer2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panelPlayer1 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownColors = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numHeight = new System.Windows.Forms.NumericUpDown();
+            this.numWidth = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(301, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Игрок 2:";
+            // 
+            // panelPlayer2
+            // 
+            this.panelPlayer2.Location = new System.Drawing.Point(304, 249);
+            this.panelPlayer2.Name = "panelPlayer2";
+            this.panelPlayer2.Size = new System.Drawing.Size(39, 39);
+            this.panelPlayer2.TabIndex = 40;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Игрок 1:";
+            // 
+            // panelPlayer1
+            // 
+            this.panelPlayer1.Location = new System.Drawing.Point(70, 249);
+            this.panelPlayer1.Name = "panelPlayer1";
+            this.panelPlayer1.Size = new System.Drawing.Size(39, 39);
+            this.panelPlayer1.TabIndex = 38;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(37, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 60);
+            this.panel1.TabIndex = 37;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 27);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownColors
+            // 
+            this.numericUpDownColors.Location = new System.Drawing.Point(261, 100);
+            this.numericUpDownColors.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownColors.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDownColors.Name = "numericUpDownColors";
+            this.numericUpDownColors.Size = new System.Drawing.Size(34, 20);
+            this.numericUpDownColors.TabIndex = 35;
+            this.numericUpDownColors.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Выбирите количество цветов";
+            // 
+            // numHeight
+            // 
+            this.numHeight.Location = new System.Drawing.Point(286, 52);
+            this.numHeight.Maximum = new decimal(new int[] {
+            21,
+            0,
+            0,
+            0});
+            this.numHeight.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numHeight.Name = "numHeight";
+            this.numHeight.Size = new System.Drawing.Size(34, 20);
+            this.numHeight.TabIndex = 33;
+            this.numHeight.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // numWidth
+            // 
+            this.numWidth.Location = new System.Drawing.Point(146, 52);
+            this.numWidth.Maximum = new decimal(new int[] {
+            21,
+            0,
+            0,
+            0});
+            this.numWidth.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numWidth.Name = "numWidth";
+            this.numWidth.Size = new System.Drawing.Size(34, 20);
+            this.numWidth.TabIndex = 32;
+            this.numWidth.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Высота:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Ширина:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Выбирите размер поля:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 363);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panelPlayer2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panelPlayer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDownColors);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numHeight);
+            this.Controls.Add(this.numWidth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Начало Игры";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panelPlayer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panelPlayer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownColors;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numHeight;
+        private System.Windows.Forms.NumericUpDown numWidth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ColorGame/ColorGame/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorGame
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (Graphics g = e.Graphics)
+            {
+                DrowChooseBox(g, new Point(0, 0), Color.Black);
+            }
+        }
+
+        private void DrowChooseBox(Graphics g, Point p, Color color)
+        {
+            Pen blackpen = new Pen(Color.Black, 3);
+
+            var brush = new SolidBrush(Color.Black);
+
+            var rectangle = new Rectangle(0, 0, 30, 30);
+
+            g.DrawRectangle(blackpen, rectangle);
+            g.FillRectangle(brush, rectangle);
+        
+        }
+    }
+}
